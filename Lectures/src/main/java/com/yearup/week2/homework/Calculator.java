b26969fd3b9c53c2f6e95965b0c6c631cf4629ae
LAPTOP-CTHNC5P1\ssujo <khanshahnia@gmail.com>
2022-11-12 17:00:21 -0500

    created interface and class

---
+++ Lectures/src/main/java/com/yearup/week2/homework/Calculator.java
@@ -0,0 +1,15 @@
+package main.java.com.yearup.week2.homework;
+
+public interface Calculator {
+
+
+    public double calculateArea(double x, double y);
+
+
+    public double calculateSum(double i, double j);
+
+
+    public double calculateVolume(double s, double h, double k);
+
+
+}
