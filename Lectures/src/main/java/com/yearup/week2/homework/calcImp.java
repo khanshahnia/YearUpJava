8488079f974529c4bed66ec67e6179483b457064
Shahnia <khanshahnia@gmail.com>
2022-11-12 23:30:24 -0500

    Added Screenshot

--- Lectures/src/main/java/com/yearup/week2/homework/calcImp.java
+++ Lectures/src/main/java/com/yearup/week2/homework/calcImp.java
@@ -1,31 +1,35 @@
 package com.yearup.week2.homework;
+
 import java.util.Scanner;
+
 public class calcImp implements Calculator {
 
     private double returnValue;
     private double area_square;
-    public calcImp(){ //constructor
+
+    public calcImp() { //constructor
 
     }
 
 
-    public  double getInput(){
-        Scanner sc= new Scanner(System.in) ;
+    public double getInput() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length : ");
 
-        double userInput= sc.nextInt();
+        double userInput = sc.nextInt();
         //System.out.println(userInput);
 
         //double area_square=userInput*userInput;
         //System.out.println("Area is: "+area_square);
         return userInput;
     }
+
     @Override
     public double calculateArea(double r, double y) {
 
-       area_square=r*r;
+        area_square = r * r;
 
-      return 0;
+        return 0;
     }
 
     @Override
@@ -37,8 +41,9 @@ public class calcImp implements Calculator {
     public double calculateVolume(double s, double h, double k) {
         return 0;
     }
-    public void displayArea(){
-        System.out.println("Area is: "+area_square);
+
+    public void displayArea() {
+        System.out.println("Area is: " + area_square);
     }
 
 }
--- Lectures/src/main/java/com/yearup/week2/homework/calculationTest.java
+++ Lectures/src/main/java/com/yearup/week2/homework/calculationTest.java
@@ -2,16 +2,12 @@ package com.yearup.week2.homework;
 
 public class calculationTest {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         calcImp object = new calcImp();
         final double length = object.getInput();
 
-        final double area = object.calculateArea(length,length);
+        final double area = object.calculateArea(length, length);
         object.displayArea();
-
-
-//        Integer x = new Integer(10);
-//        int x = 10;
 
 
     }
